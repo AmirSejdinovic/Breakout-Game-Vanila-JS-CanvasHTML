98131df74171bf993961f2bbe81b50805158746e
AmirSejdinovic <37899933+AmirSejdinovic@users.noreply.github.com>
2020-05-15 12:05:44 +0200

    Move the ball and break bricks

--- script.js
+++ script.js
@@ -102,6 +102,48 @@ function movePaddle() {
     paddle.x = 0;
   }
 }
+//Move ball on canvas
+function moveBall() {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  //Wall collision (right/left)
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+    ball.dx *= -1; //ball.dx = ball.dx * -1
+  }
+
+  //Wall collision (top/bottom)
+  if (ball.y - ball.size > canvas.height || ball.y - ball.size < 0) {
+    ball.dy *= -1;
+  }
+  //console.log(ball.x, ball.y);
+
+  //Paddle collison
+  if (
+    ball.x - ball.size > paddle.x &&
+    ball.x + ball.size < paddle.x + paddle.w &&
+    ball.y + ball.size > paddle.y
+  ) {
+    ball.dy = -ball.speed;
+  }
+
+  //Brick collison
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      if (brick.visible) {
+        if (
+          ball.x - ball.size > brick.x &&
+          ball.x + ball.size < brick.x + brick.w &&
+          ball.y + ball.size > brick.y &&
+          ball.y - ball.size < brick.y + brick.h
+        ) {
+          ball.dy *= -1;
+          brick.visible = false;
+        }
+      }
+    });
+  });
+}
 
 //Draw everything
 function draw() {
@@ -116,6 +158,7 @@ function draw() {
 //Update canvas drawing and animation
 function update() {
   movePaddle();
+  moveBall();
 
   //Draw everything
   draw();
